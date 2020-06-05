3e44cd11b7a8a96f05410f46f2cb2ea15bd901e4
Tanya Hodges <tanyac52003@gmail.com>
2020-06-05 18:00:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,36 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |dwarf, index|
+    puts "#{index + 1}. #{dwarf}"
+  end 
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)
+ calls.map! {|call| call.capitalize + "!"}
+end 
+
+
+def long_planeteer_calls(calls)
+  if calls.any? {|call| call.length > 4}
+    true 
+  else
+    false 
+  end 
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-end
+   cheese_types.find {|array| array.include? (array)}
+end 
+  
+
+
+
+
+
+
+
+
+
+
